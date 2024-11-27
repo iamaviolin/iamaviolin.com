32cf7608de5019722726573accb3f182f47572dd
Rain <rain@synth.kitchen>
2024-11-27 13:38:20 +0100

    turn off the analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import { analyzer } from "vite-bundle-analyzer";
+// import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(), analyzer()],
+  plugins: [preact()],
 });
