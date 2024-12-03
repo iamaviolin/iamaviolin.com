001c427a92eb986e7785219d94914e0a77947975
Rain <rain@synth.kitchen>
2024-12-03 09:44:42 +0100

    make the links at least presentable

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -13,52 +13,54 @@ export const ContactRoute = () => (
           <source type="image/webp" srcset={contactWebp} />
           <img
             src={contactPng}
-            alt="Kathleen Chen working front of house, looking quite cheerful!"
+            alt="Soundcheck for C'est Karma, from front-of-house"
           />
         </picture>
       </div>
 
-      <article>
-        <h2>links</h2>
+      <div>
+        <article>
+          <h2>links</h2>
 
-        <ul>
-          <li>
-            <a href="mailto:iamaviolin@gmail.com">email</a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/rosemead/" target="_blank">
-              linkedin
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/audio.kat/" target="_blank">
-              instagram
-            </a>
-          </li>
-        </ul>
-      </article>
+          <ul>
+            <li>
+              <a href="mailto:iamaviolin@gmail.com">email</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/rosemead/" target="_blank">
+                linkedin
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/audio.kat/" target="_blank">
+                instagram
+              </a>
+            </li>
+          </ul>
+        </article>
 
-      <article>
-        <h2>organizations</h2>
+        <article>
+          <h2>organizations</h2>
 
-        <ul>
-          <li>
-            <a href="https://soundsysters.com/" target="_blank">
-              soundsysters
-            </a>
-          </li>
-          <li>
-            <a href="https://womeninlivemusic.eu/" target="_blank">
-              women in live music
-            </a>
-          </li>
-          <li>
-            <a href="https://soundgirls.org/" target="_blank">
-              soundgirls
-            </a>
-          </li>
-        </ul>
-      </article>
+          <ul>
+            <li>
+              <a href="https://soundsysters.com/" target="_blank">
+                soundsysters
+              </a>
+            </li>
+            <li>
+              <a href="https://womeninlivemusic.eu/" target="_blank">
+                women in live music
+              </a>
+            </li>
+            <li>
+              <a href="https://soundgirls.org/" target="_blank">
+                soundgirls
+              </a>
+            </li>
+          </ul>
+        </article>
+      </div>
     </section>
   </main>
 );
