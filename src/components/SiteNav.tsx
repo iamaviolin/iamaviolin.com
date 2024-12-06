58462dde1979ab4401beff8140c510eadb312a31
Rain <rain@synth.kitchen>
2024-12-06 12:58:17 +0100

    add a grid icon to the home page link

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -1,12 +1,17 @@
 import { FunctionComponent } from "preact";
 import { Link, useRoute } from "wouter-preact";
 
+import grid from "../assets/grid.svg";
+
 export const SiteNav: FunctionComponent = () => {
   const isHome = useRoute("/")[0];
 
   return isHome ? null : (
     <nav>
-      <Link href="/">home</Link>
+      <Link href="/">
+        <img role="presentation" src={grid} />
+        home
+      </Link>
     </nav>
   );
 };
